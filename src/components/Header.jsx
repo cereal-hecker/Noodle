ecd33c99497b82b1f1664fc54036af86d895e064
Riya Batla <Riya Batla>
2022-10-09 22:23:34 +0530

    style improved

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,7 +28,7 @@ export default function Header() {
         <div>
         <div class="px-6 py-7">
         <div class="mb-3 lg:w-3/5 sm:w-3/4 flex">
-            <h1 class="font-serif text-4xl pr-7">nOOdle</h1>
+            <h1 class="text-5xl pr-7 brand">NOODLE</h1>
             <input type="search" value="football" class="form-control rounded-l-full w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon2" />
             <button class="rounded-r-full btn inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out items-center" type="button" id="button-addon2">
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,10 +4,10 @@ export default function Result(props) {
 
     return (
     <div>
-        <div class="px-6 py-7 w-100">
+        <div class="px-6 py-7">
             <a href={props.link}>
-                <h3 class="font-bold text-lg">{props.title}</h3>
                 <h7 class="text-gray-400 text-sm">{props.displayed_link}</h7>
+                <h3 class="font-bold text-lg hover:underline text-blue-700">{props.title}</h3>
                 <p>{props.snippet}</p>
             </a>
         </div>
